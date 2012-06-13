aca2ec669250b514a81c8596dec2b277740ce61e
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-06-13 15:38:17 -0400

    cleanup

--- CopyFontsThere.sh
+++ CopyFontsThere.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "This script copys the files in the Git directory to the users .fonts directory, creating it if necessary."
 #cp -R -v * $HOME/.fonts/
-find ./ ! -name '*.sh' | xargs -i cp -R -v {} $HOME/.fonts/
+#find ./ ! -name '*.sh' | xargs -i cp -R -v {} $HOME/.fonts/ #aggh this copied all the git files as well.
+cp -R -v *.[tT][Tt][fF] $HOME/.fonts/
